5fe23567c2efe150e2159dad3266dbcbd8346313
iewad <lidawei.me@gmail.com>
2018-09-23 09:33:31 +0800

    Add User Sys. with Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '/',
+    controllers: {
+      sessions: 'devise/sessions',
+      registrations: 'devise/registrations',
+      passwords: 'devise/passwords'
+    }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
 end
