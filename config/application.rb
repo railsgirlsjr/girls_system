675a325189c69fbb4e2afb7b42bb6eaa2b47b956
iewad <lidawei.me@gmail.com>
2018-09-23 11:06:18 +0800

    Devise i18n

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,11 @@ module GirlsSystem
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.i18n.default_locale = :en
+    config.i18n.available_locales = %i[zh-CN zh en]
+    config.i18n.enforce_available_locales = true
+
     config.generators do |g|
       # g.test_framework  :rspec, view_specs: false, helper_specs: false
       g.stylesheets     false
